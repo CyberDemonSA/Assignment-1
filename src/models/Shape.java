b10a95cb2a0c91de253a8f8363af5dbf0926669c
User <230519@astanait.edu.kz>
2023-12-24 17:51:59 +0600

    in process 6

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,15 +1,23 @@
 package models;
 
 import java.util.ArrayList;
+
 public class Shape {
-    public ArrayList<Point> ArrayListPoint;
+    private ArrayList<Point> ArrayListPoint;
     public ArrayList<Double> side;
 
     public void addPoint(Point point){
         ArrayListPoint.add(point);
     }
 
-    public ArrayList<Double> distance
+    public void distanceTo(ArrayList<Point> ArrayListPoint){
+        int i = 0;
+        while (ArrayListPoint.size()-1 > i){
+            double a=ArrayListPoint[i].x-ArrayListPoint[i+1].x;
+            double b=ArrayListPoint[i].y-ArrayListPoint[i+1].y;
+            side.add(Math.sqrt());
+        }
+    }
 
     // calculatePerimeter()
 
